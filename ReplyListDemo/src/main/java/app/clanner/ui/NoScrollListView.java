9f903f31cd4e06b6871b835b0bfd5cb058453de2
Clanner <371048852@qq.com>
2016-08-26 10:55:06 +0800

    评论回复Demo

---
+++ ReplyListDemo/src/main/java/app/clanner/ui/NoScrollListView.java
@@ -0,0 +1,18 @@
+package app.clanner.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by Clanner on 2016/8/26.
+ */
+public class NoScrollListView extends ListView{
+    public NoScrollListView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        int mExpandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, mExpandSpec);
+    }
+}
